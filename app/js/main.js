bae30e57797f81b106341fa5996b88b5ff8c976e
LeoKikosh <crazy.kikosh@gmail.com>
2024-11-05 22:38:24 +0300

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,22 @@
 $(function () {
+
+  $('.product-item__input').styler();
+
+  $('.product-slider__thumb').slick({
+    asNavFor: '.product-slider__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+  $('.product-slider__big').slick({
+    asNavFor: '.product-slider__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
   $('.star').rateYo({
     starWidth: "11px",
     normalFill: "#d6d6d6",
@@ -6,6 +24,12 @@ $(function () {
   });
 
   $('.star-shop').rateYo({
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00"
+  });
+
+  $('.product__star').rateYo({
     starWidth: "18px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00"
